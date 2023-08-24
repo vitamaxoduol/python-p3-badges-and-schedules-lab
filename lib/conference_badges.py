8567a4d478a409863399ad5ea419405117a9e351
Vitamax <vitamaxoduo@gmail.com>
2023-08-24 23:43:53 +0300

    Completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,13 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]  # using list comprehension
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {i + 1}!" for i, name in enumerate(names)]
 
 def printer(names):
-    return None
+    all_messages = batch_badge_creator(names) + assign_rooms(names)
+    for message in all_messages:
+        print(message)
